79305f03b92e23c98564c85df9781eb3bd68724d
yamavol <yamavol@outlook.com>
2025-05-20 19:02:50 +0900

    Replace with cobapen eslint-confing

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,13 @@
-import stylistic from "@stylistic/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import importPlugin from "eslint-plugin-import";
+import { configs, parserConfigs } from "@cobapen/eslint-config";
+import { defineConfig } from "eslint/config";
 
-export default [
+export default defineConfig(
   {
     ignores: [
+      "lib",
       "node_modules/",
     ],
   },
-  {
-    files: ["**/*.ts"],
-    languageOptions: {
-      parser: tsParser,
-    }
-  },
-  {
-    plugins: {
-      "@stylistic": stylistic,
-      "@import": importPlugin,
-    },
-    files: ["**/*.js", "**/*.ts"],
-    rules: {
-      "@stylistic/semi": "warn",
-      "@stylistic/quotes": ["warn", "double"],
-      "@stylistic/indent": ["warn", 2],
-      "@import/order": ["warn", {
-        groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
-        alphabetize: { order: "asc", caseInsensitive: true },
-        named: true,
-      }],
-    },
-  },
-];
+  parserConfigs.typescript.recommended,
+  configs.recommended,
+);
